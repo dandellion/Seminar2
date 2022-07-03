05c9820951609d6cd98be6df6cd23da9143d712f
Tasa <greatwillow@mail.ru>
2022-07-03 16:33:27 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            if (result<1000 && result>99)
+                break;
+            else {
+                Console.Clear();
+                Console.WriteLine("Вы ввели не треззначное число. Введите корректное число.");
+                }
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Введите корректное число.");
+        }
+
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число");
+
+int secondN = (number / 10) % 10;
+
+Console.WriteLine("Полученное число {0}",secondN);
